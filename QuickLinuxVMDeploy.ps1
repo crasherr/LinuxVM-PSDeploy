5f78474d14f231ad0e2eca29d54702e22ef9a115
Steve Pataki <stevepataki@yahoo.com>
2017-06-05 15:05:17 -0400

    Added PS file QuickLinuxVMDeploy.ps1

---
+++ QuickLinuxVMDeploy.ps1
@@ -0,0 +1,79 @@
+﻿# Create resource group
+New-AzureRmResourceGroup -Name myResourceGroup -Location eastus
+
+# Create a subnet configuration
+$subnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name mySubnet -AddressPrefix 192.168.1.0/24
+
+# Create a virtual network
+$vnet = New-AzureRmVirtualNetwork -ResourceGroupName myResourceGroup -Location eastus `
+-Name MYvNET -AddressPrefix 192.168.0.0/16 -Subnet $subnetConfig
+
+# Create a public IP address and specify a DNS name
+$pip = New-AzureRmPublicIpAddress -ResourceGroupName myResourceGroup -Location eastus `
+-AllocationMethod Static -IdleTimeoutInMinutes 4 -Name "mypublicdns$(Get-Random)"
+
+# Create an inbound network security group rule for port 22
+$nsgRuleSSH = New-AzureRmNetworkSecurityRuleConfig -Name myNetworkSecurityGroupRuleSSH  -Protocol Tcp `
+-Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * `
+-DestinationPortRange 22 -Access Allow
+
+# Create an inbound network security group rule for port 80
+$nsgRuleWeb = New-AzureRmNetworkSecurityRuleConfig -Name myNetworkSecurityGroupRuleWWW  -Protocol Tcp `
+-Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * `
+-DestinationPortRange 80 -Access Allow
+
+# Create a network security group
+$nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName myResourceGroup -Location eastus `
+-Name myNetworkSecurityGroup -SecurityRules $nsgRuleSSH,$nsgRuleWeb
+
+# Create a virtual network card and associate with public IP address and NSG
+$nic = New-AzureRmNetworkInterface -Name myNic -ResourceGroupName myResourceGroup -Location eastus `
+-SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+
+# Define a credential object
+$securePassword = ConvertTo-SecureString ' ' -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ("azureuser", $securePassword)
+
+# Create a virtual machine configuration
+$vmConfig = New-AzureRmVMConfig -VMName myVM -VMSize Standard_D1 | `
+Set-AzureRmVMOperatingSystem -Linux -ComputerName myVM -Credential $cred -DisablePasswordAuthentication | `
+Set-AzureRmVMSourceImage -PublisherName Canonical -Offer UbuntuServer -Skus 14.04.2-LTS -Version latest | `
+Add-AzureRmVMNetworkInterface -Id $nic.Id
+
+# Configure SSH Keys
+$sshPublicKey = Get-Content "$env:USERPROFILE\.ssh\id_rsa.pub"
+Add-AzureRmVMSshPublicKey -VM $vmconfig -KeyData $sshPublicKey -Path "/home/azureuser/.ssh/authorized_keys"
+
+# create virtual machine
+New-AzureRmVM -ResourceGroupName myResourceGroup -Location eastus -VM $vmConfig
+
+# create SSH connection with virtual machine - 1 + 2
+
+# 1. return public ip address of vm
+Get-AzureRmPublicIpAddress -ResourceGroupName myResourceGroup | Select IpAddress
+
+# 2. From a system with SSH installed, used the following command to connect to the virtual machine. 
+# If working on Windows, Putty can be used to create the connection. 
+
+# (using bash)
+ssh <Public IP Address>
+
+# When prompted, the login user name is azureuser. If a passphrase was entered when creating SSH keys, you need to enter this as well.
+
+# Install NGINX - Use the following bash script to update package sources and install the latest NGINX package.
+
+# (using bash)
+#!/bin/bash
+
+# update package source
+apt-get -y update
+
+# install NGINX
+apt-get -y install nginx
+
+# View the NGIX welcome page (go to public ip address)
+# With NGINX installed and port 80 now open on your VM from the Internet - you can use a web browser of your choice 
+# to view the # default NGINX welcome page. Be sure to use the public IP address you documented above to visit the default page.
+
+# Clean up resources
+Remove-AzureRmResourceGroup -Name myResourceGroup
